66ca993cf8d250c97ac36e38ca335142114963a0
Voica Mihai <mickai55@yahoo.com>
2024-01-29 15:30:36 +0200

    add display answers

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,7 +1,162 @@
-import React from "react";
+"use client";
 
-const Answer = () => {
-  return <div>Answer</div>;
+import React, { BaseSyntheticEvent, useRef, useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { AnswersSchema } from "@/lib/validations";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Editor } from "@tinymce/tinymce-react";
+import { useTheme } from "@/context/ThemeProvider";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { createAnswer } from "@/lib/actions/answer.action";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  question: string;
+  questionId: string;
+  authorId: string;
+}
+
+const Answer = ({ question, questionId, authorId }: Props) => {
+  const pathname = usePathname();
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const { mode } = useTheme();
+  const editorRef = useRef(null);
+  const form = useForm<z.infer<typeof AnswersSchema>>({
+    resolver: zodResolver(AnswersSchema),
+    defaultValues: {
+      answer: "",
+    },
+  });
+
+  async function handleCreateAnswer(values: z.infer<typeof AnswersSchema>) {
+    setisSubmitting(true);
+    try {
+      await createAnswer({
+        content: values.answer,
+        author: JSON.parse(authorId),
+        question: JSON.parse(questionId),
+        path: pathname,
+      });
+
+      form.reset();
+      const editor = editorRef.current as any;
+      if (editor) {
+        editor.setContent("");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setisSubmitting(false);
+    }
+  }
+
+  return (
+    <div>
+      <div
+        className="flex flex-col justify-between gap-5 sm:flex-row
+        sm:items-center sm:gap-2"
+      >
+        <h4 className="paragraph-semibold text-dark-400_light800">
+          Write your answer here
+        </h4>
+        <Button
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 
+        text-primary-500 shadow-none dark:text-primary-500"
+          onClick={() => {}}
+        >
+          <Image
+            src="/assets/icons/stars.svg"
+            width={12}
+            height={12}
+            alt="pen"
+            className="object-contain"
+          />
+          Generate an AI answer
+        </Button>
+      </div>
+      <Form {...form}>
+        <form
+          className="mt-6 flex w-full flex-col gap-10"
+          onSubmit={form.handleSubmit(handleCreateAnswer)}
+        >
+          <FormField
+            control={form.control}
+            name="answer"
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col gap-3">
+                <FormControl className="mt-3.5">
+                  <Editor
+                    onKeyPress={(e) => {
+                      if (editorRef.current)
+                        form.setValue(
+                          "answer",
+                          // @ts-ignore
+                          editorRef.current.getContent()
+                        );
+                    }}
+                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                    onInit={(evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor;
+                    }}
+                    initialValue=""
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "image",
+                        "charmap",
+                        "preview",
+                        "anchor",
+                        "searchreplace",
+                        "visualblocks",
+                        "codesample",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                      ],
+                      toolbar:
+                        "undo redo | " +
+                        "codesample | bold italic forecolor | alignleft aligncenter | " +
+                        "alignright alignjustify | bullist numlist",
+                      content_style:
+                        "body { font-family:Inter; font-size:16px }",
+                      skin: mode === "dark" ? "oxide-dark" : "oxide",
+                      content_css: mode === "dark" ? "dark" : "light",
+                    }}
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              className="primary-gradient w-fit text-white"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default Answer;
---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import Answer from "@/database/answer.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import Question from "@/database/question.model";
+import { revalidatePath } from "next/cache";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { author, question, content, path } = params;
+    const newAnswer = await Answer.create({
+      author,
+      question,
+      content,
+    });
+
+    // add the answer to the question answer array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    // TODO: add interaction...
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
